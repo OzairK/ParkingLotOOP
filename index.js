6907743774cc5cc47a51a07e4b82881931107ca6
ozair khan <ozairsaddress@gmail.com>
2024-08-09 11:40:03 -0500

    cleaning up code

--- index.js
+++ index.js
@@ -1,99 +1,110 @@
-class car {
+class Car {
   constructor(license, checkInTime) {
     this.license = license;
-    this.checkedIn = checkInTime
-    this.feeCharged = 0
-    this.checkoutTime = undefined
+    this.checkInTime = checkInTime || new Date();
+    this.feeCharged = 0;
+    this.checkOutTime = null;
   }
 
-  checkOut(checkoutTime, fee) {
-    const hoursParked = (checkoutTime - this.checkedIn)/ (1000*60*60)
-    this.checkoutTime = checkoutTime
-    this.feeCharged = fee * hoursParked
+  checkOut(checkOutTime, rate) {
+    const hoursParked = (checkOutTime - this.checkInTime) / (1000 * 60 * 60);
+    this.checkOutTime = checkOutTime;
+    this.feeCharged = rate * hoursParked;
   }
 }
-
 
 class ParkingLot {
-  constructor(name, fee) {
-    this.name = name
-    this.fee = fee
-    this.cars = {}
-    this.totalMade = 0
+  constructor(name, rate) {
+    this.name = name;
+    this.rate = rate;
+    this.cars = {};
+    this.totalRevenue = 0;
   }
 
-  addCar(licenseNumber, checkInTime) {
-    const timeCheckedIn = checkInTime || new Date()
-    const newCar = new car(licenseNumber, timeCheckedIn)
-    if(this.cars[licenseNumber]) {
-      console.log('car already exists')
-      return false
+  addCar(license, checkInTime) {
+    if (this.cars[license]) {
+      console.log('Car is already checked in.');
+      return false;
     }
-    console.log('car checked in at', newCar.checkedIn)
-    this.cars[licenseNumber] = newCar
+    const car = new Car(license, checkInTime);
+    this.cars[license] = car;
+    console.log(`Car ${license} checked in at ${car.checkInTime}`);
+    return true;
   }
 
-  removeCar(licenseNumber, checkoutTime) {
-    const car = this.cars[licenseNumber]
-    if(!car) {
-      console.log('car is not checked in')
-      return false
+  removeCar(license, checkOutTime) {
+    const car = this.cars[license];
+    if (!car) {
+      console.log('Car is not checked in.');
+      return false;
     }
-    car.checkOut(checkoutTime, this.fee)
-    this.totalMade = car.feeCharged + this.totalMade
-    console.log('car has been checked out and charged', car.feeCharged)
-    delete this.cars[licenseNumber]
+    car.checkOut(checkOutTime, this.rate);
+    this.totalRevenue += car.feeCharged;
+    console.log(`Car ${license} checked out and charged $${car.feeCharged.toFixed(2)}`);
+    delete this.cars[license];
+    return true;
   }
 }
-
 
 class ParkingGarageManagement {
-  constructor(){
-    this.parkingLots = {}
+  constructor() {
+    this.parkingLots = {};
   }
 
-  addParkingGarage(name, fee){
-    const newGarage = new ParkingLot(name, fee)
-    if(this.parkingLots[fee]) {
-      console.log('parking garage already exists')
-      return false
+  addParkingLot(name, rate) {
+    if (this.parkingLots[name]) {
+      console.log('Parking lot already exists.');
+      return false;
     }
-    this.parkingLots[name] = newGarage
+    this.parkingLots[name] = new ParkingLot(name, rate);
+    return true;
   }
 
-  addCar(parkingLotName,licenseNumber, checkInTime) {
-    const garage = this.parkingLots[parkingLotName]
-    if(!garage) {
-      console.log('garage does not exist')
-      return false
+  addCar(parkingLotName, license, checkInTime) {
+    const lot = this.parkingLots[parkingLotName];
+    if (!lot) {
+      console.log('Parking lot does not exist.');
+      return false;
     }
-    garage.addCar(licenseNumber, checkInTime)
+    return lot.addCar(license, checkInTime);
   }
 
-  removeCar(parkingLotName,licenseNumber, checkoutTime) {
-    const garage = this.parkingLots[parkingLotName]
-    if(!garage) {
-      console.log('garage does not exist')
-      return false
+  removeCar(parkingLotName, license, checkOutTime) {
+    const lot = this.parkingLots[parkingLotName];
+    if (!lot) {
+      console.log('Parking lot does not exist.');
+      return false;
     }
-    garage.removeCar(licenseNumber, checkoutTime)
+    return lot.removeCar(license, checkOutTime);
   }
-  
-  fetchHighestRevenueGenerator() {
-    const parkingLotArr = Object.values(this.parkingLots)
-    const parkingLotsSorted = parkingLotArr.sort((a,b) => b.totalMade - a.totalMade)
-    return parkingLotsSorted[0].name 
+
+  getHighestRevenueLot() {
+    let highestRevenueLot = null;
+    let maxRevenue = 0;
+
+    for (const lot of Object.values(this.parkingLots)) {
+      if (lot.totalRevenue > maxRevenue) {
+        highestRevenueLot = lot;
+        maxRevenue = lot.totalRevenue;
+      }
+    }
+
+    return highestRevenueLot ? highestRevenueLot.name : null;
   }
 }
 
-const houstonParkingManagement = new ParkingGarageManagement()
-houstonParkingManagement.addParkingGarage('south side', 10)
-houstonParkingManagement.addParkingGarage('north side', 5)
-houstonParkingManagement.addCar('south side', 'abc', new Date('Fri Aug 09 2024 10:56:22 GMT-0500 (Central Daylight Time)'))
-houstonParkingManagement.addCar('south side', 'abcd', new Date('Fri Aug 09 2024 10:58:22 GMT-0500 (Central Daylight Time)'))
-houstonParkingManagement.addCar('north side', 'xyz', new Date('Fri Aug 09 2024 10:58:22 GMT-0500 (Central Daylight Time)'))
-houstonParkingManagement.removeCar('south side', 'abc', new Date('Fri Aug 09 2024 12:56:22 GMT-0500 (Central Daylight Time)'))
-houstonParkingManagement.removeCar('south side', 'abcd', new Date('Fri Aug 09 2024 12:58:22 GMT-0500 (Central Daylight Time)'))
-houstonParkingManagement.removeCar('north side', 'xyz', new Date('Fri Aug 10 2024 12:58:22 GMT-0500 (Central Daylight Time)'))
-console.log(`parking lot that made most revenue: ${houstonParkingManagement.fetchHighestRevenueGenerator()}`)
-console.log(houstonParkingManagement.parkingLots)
+// Example usage
+const parkingManagement = new ParkingGarageManagement();
+parkingManagement.addParkingLot('South Side', 10);
+parkingManagement.addParkingLot('North Side', 5);
+
+parkingManagement.addCar('South Side', 'ABC123', new Date('2024-08-09T10:56:22-05:00'));
+parkingManagement.addCar('South Side', 'XYZ789', new Date('2024-08-09T10:58:22-05:00'));
+parkingManagement.addCar('North Side', 'LMN456', new Date('2024-08-09T10:58:22-05:00'));
+
+parkingManagement.removeCar('South Side', 'ABC123', new Date('2024-08-09T12:56:22-05:00'));
+parkingManagement.removeCar('South Side', 'XYZ789', new Date('2024-08-09T12:58:22-05:00'));
+parkingManagement.removeCar('North Side', 'LMN456', new Date('2024-08-10T12:58:22-05:00'));
+
+console.log(`Parking lot with the highest revenue: ${parkingManagement.getHighestRevenueLot()}`);
+console.log(parkingManagement.parkingLots);
